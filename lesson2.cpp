0e3170fca0c717838f95f2cf743ba307222cb2de
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2023-09-22 17:33:18 +0530

    area of triangle

---
+++ lesson2.cpp
@@ -0,0 +1,15 @@
+// Write a programe to findout area of triangle 
+#include<iostream>
+using namespace std;
+int main()
+{
+    float height,base;
+    float answer ; 
+    cout<<"Enter value of height ";
+    cin>>height;
+    cout<<"Enter value of base ";
+    cin>>base;
+    
+    answer = height * base / 2 ;
+    cout<<"The value of answer is "<<answer;
+}
