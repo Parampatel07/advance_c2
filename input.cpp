922afbe7cddc905308f00610ad91b7656166b210
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2023-09-22 17:21:54 +0530

    example of input

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number1 ;
+    float number2 ;
+    cout<<"Enter value of number 1 ";
+    cin>>number1;
+    cout<<"Enter value of number 2 ";
+    cin>>number2;
+    
+    cout<<"The value of number 1 is "<<number1<<endl;
+    cout<<"The value of number 2 is "<<number2;
+}
