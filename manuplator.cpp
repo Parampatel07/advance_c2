3a511c7250a686b00e49fc68931ada1b584c175f
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2023-09-26 16:37:55 +0530

    manuplators

---
+++ manuplator.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    cout<<setw(10)<<"My name is Param Patel"<<endl;
+    cout<<setw(20)<<"Hello world "<<endl;
+    cout<<setw(20)<<setfill('-')<<"Hello world";
+}
