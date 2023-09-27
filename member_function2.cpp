614d60a1ab29b230f3e3d6a452c360ffbd57aa20
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2023-09-27 16:18:42 +0530

    inside_the_class

---
+++ member_function2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class triangle
+{
+  private:
+  int height = 10, base =20 ;
+  
+  public:
+    void get_input()
+    {
+        cout<<"Enter value of height ";
+        cin>>height;
+        cout<<"Enter value of base ";
+        cin>>base;
+    }  
+    
+    void get_area()
+    {
+        int answer ;
+        answer = height * base / 2 ;
+        cout<<"The value of answer is "<<answer;
+    }
+
+};
+
+
+
+int main()
+{
+    triangle t1 ;
+    t1.get_input();
+    t1.get_area();
+}
